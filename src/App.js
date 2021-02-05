1c3f17335aff044e83affd1f4e57397fed005d46
Nathan Szurek <nathan.szurek@yahoo.com>
2021-02-05 16:32:40 -0600

    Added image backgrounds that automatically change with new seasons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react';
 import summer from './Assets/Grass.jpg';
 import winter from './Assets/Snow.jpg';
+import spring from './Assets/Spring.jpg';
+import fall from './Assets/Fall.jpg';
 import Background from './Components/Background';
 
 function App() {
+const [season, setSeason] = useState('')
+
+useEffect(() => {
+  var currentDate = `${new Date()}`;
+  if (currentDate.includes('Dec') || currentDate.includes('Jan') || currentDate.includes('Feb')) {
+    setSeason(winter)
+  } else if (currentDate.includes('Mar') || currentDate.includes('Apr') || currentDate.includes('May')) {
+    setSeason(spring)
+  } else if (currentDate.includes('Jun') || currentDate.includes('Jul') || currentDate.includes('Aug')) {
+    setSeason(summer)
+  } else if (currentDate.includes('Sep') || currentDate.includes('Oct') || currentDate.includes('Nov')) {
+    setSeason(fall)
+  }
+  
+}, [])
 
   return (
     <div>
-      <Background src={summer} alt='summer'/>
+      {season === summer? <Background src={summer} alt='Summer image'/> : null}
+      {season === winter? <Background src={winter} alt='Winter image'/> : null}
+      {season === spring? <Background src={spring} alt='Spring image'/> : null}
+      {season === fall? <Background src={fall} alt='Fall image'/> : null}
     </div>
   );
 }
