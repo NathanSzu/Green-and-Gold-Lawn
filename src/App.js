a1aa4b954dd1e1c835076515ce803359d0ddd996
Nathan Szurek <nathan.szurek@yahoo.com>
2021-02-05 17:08:44 -0600

    Altered code for better gsap animation on season transitions

--- src/App.js
+++ src/App.js
@@ -4,30 +4,46 @@ import winter from './Assets/Snow.jpg';
 import spring from './Assets/Spring.jpg';
 import fall from './Assets/Fall.jpg';
 import Background from './Components/Background';
+import gsap from "gsap";
 
 function App() {
-const [season, setSeason] = useState('')
+  const [season, setSeason] = useState('')
 
-useEffect(() => {
-  var currentDate = `${new Date()}`;
-  if (currentDate.includes('Dec') || currentDate.includes('Jan') || currentDate.includes('Feb')) {
-    setSeason(winter)
-  } else if (currentDate.includes('Mar') || currentDate.includes('Apr') || currentDate.includes('May')) {
-    setSeason(spring)
-  } else if (currentDate.includes('Jun') || currentDate.includes('Jul') || currentDate.includes('Aug')) {
-    setSeason(summer)
-  } else if (currentDate.includes('Sep') || currentDate.includes('Oct') || currentDate.includes('Nov')) {
-    setSeason(fall)
+  useEffect(() => {
+    var currentDate = `${new Date()}`;
+    if (currentDate.includes('Dec') || currentDate.includes('Jan') || currentDate.includes('Feb')) {
+      gsap.to('.winter', {css:{opacity:100}})
+      setSeason('winter')
+    } else if (currentDate.includes('Mar') || currentDate.includes('Apr') || currentDate.includes('May')) {
+      gsap.to('.spring', {css:{opacity:100}})
+      setSeason('spring')
+    } else if (currentDate.includes('Jun') || currentDate.includes('Jul') || currentDate.includes('Aug')) {
+      gsap.to('.summer', {css:{opacity:100}})
+      setSeason('summer')
+    } else if (currentDate.includes('Sep') || currentDate.includes('Oct') || currentDate.includes('Nov')) {
+      gsap.to('.fall', {css:{opacity:100}})
+      setSeason('fall')
+    }
+  }, [])
+
+  const toggleSeason = () => {
+    
   }
-  
-}, [])
 
   return (
     <div>
-      {season === summer? <Background src={summer} alt='Summer image'/> : null}
-      {season === winter? <Background src={winter} alt='Winter image'/> : null}
-      {season === spring? <Background src={spring} alt='Spring image'/> : null}
-      {season === fall? <Background src={fall} alt='Fall image'/> : null}
+      <div className='background summer'>
+        <Background src={summer} alt='Summer image' />
+      </div>
+      <div className='background winter'>
+        <Background src={winter} alt='Winter image' />
+      </div>
+      <div className='background spring'>
+        <Background src={spring} alt='Spring image' />
+      </div>
+      <div className='background fall'>
+        <Background src={fall} alt='Fall image' />
+      </div>
     </div>
   );
 }
--- src/Components/Background.js
+++ src/Components/Background.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Background({ src, alt }) {
     return (
-        <div className='background'>
             <img className='season-background' src={src} alt={alt}></img>
-        </div>
     )
 }
